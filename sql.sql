ec83c858d7e69428624b35802a9404df891411d4
Marcos Volpi <marcos@marcosvolpi.com>
2018-06-28 00:18:23 -0300

    Fix

--- sql.sql
+++ sql.sql
@@ -1,4 +1,20 @@
-CREATE TABLE empresas (id INTEGER PRIMARY KEY AUTOINCREMENT, nombre varchar(100) NOT NULL, descripcion varchar(255) NOT NULL);
+CREATE TABLE `empresas` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `descripcion` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+ALTER TABLE `empresas`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `empresas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+
+
+
+
+
 INSERT INTO empresas (nombre, descripcion) VALUES ('BBVA', 'Banco comercial');
 INSERT INTO empresas (nombre, descripcion) VALUES ('JP Morgan', 'Banco de inversion');
 INSERT INTO empresas (nombre, descripcion) VALUES ('Bear Stearns', 'Salvado en el 2008 fraudulentamente');
